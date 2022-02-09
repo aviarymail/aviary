213a5829afc01e4b85570ae1e2072461d1e727c3
Nick Quebbeman <n.quebb@gmail.com>
2022-02-09 09:48:07 -0800

    broke sendfn package -_-

--- apps/functions/api/introspection.ts
+++ apps/functions/api/introspection.ts
@@ -1,9 +1,46 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
+  res.status(200).json([
+    {
+      optionalProperties: {
+        category: {},
+        user: {
+          optionalProperties: {
+            contact: {
+              optionalProperties: {
+                email: {},
+              },
+            },
+          },
+          properties: {
+            name: {},
+          },
+        },
+      },
+      properties: {
+        organization: {},
+      },
+    },
+    {
+      optionalProperties: {
+        category: {},
+        user: {
+          optionalProperties: {
+            contact: {
+              optionalProperties: {
+                email: {},
+              },
+            },
+          },
+          properties: {
+            name: {},
+          },
+        },
+      },
+      properties: {
+        organization: {},
+      },
+    },
+  ]);
 }
--- packages/sendfn/src/commands/generate.ts
+++ packages/sendfn/src/commands/generate.ts
@@ -1,4 +1,5 @@
 import { command } from 'cleye';
+import fetch from 'node-fetch';
 import task from 'tasuku';
 
 export const generate = command(
@@ -11,15 +12,22 @@ export const generate = command(
   },
   async argv => {
     await task('Building @aviarymail/client', async ({ task }) => {
-      const asd = await task('Parsing configuration', async ({ setTitle, setStatus }) => {
-        await new Promise(res => setTimeout(res, 2000));
-      });
+      const { result: config } = await task(
+        'Parsing configuration',
+        async ({ setTitle, setStatus }) => {
+          return {};
+        }
+      );
 
-      await task('Fetching projects', async ({ setTitle, setStatus }) => {
-        await new Promise(res => setTimeout(res, 2000));
-      });
+      const { result: projects } = await task(
+        'Fetching projects',
+        async ({ setTitle, setStatus }) => {
+          return fetch('http://localhost:4000/api/introspection').then(res => res.json());
+        }
+      );
 
       await task('Building project interfaces', async ({ setTitle, setStatus }) => {
+        console.log(projects);
         await new Promise(res => setTimeout(res, 2000));
       });
 
